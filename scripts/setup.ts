e5a2efa96d92e4526b04a79ec5b0415eab8bac9a
cstrnt <tim.raderschad@gmail.com>
2022-11-02 08:29:28 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ scripts/setup.ts
@@ -0,0 +1,24 @@
+import { Tigris } from '@tigrisdata/core';
+import { loadEnvConfig } from '@next/env';
+import path from 'path';
+
+// Run the config loader only when not executing within next runtime
+if (process.env.NODE_ENV === undefined) {
+  if (process.env.APP_ENV === 'development') {
+    loadEnvConfig(process.cwd(), true);
+  } else if (process.env.APP_ENV === 'production') {
+    loadEnvConfig(process.cwd());
+  }
+}
+
+async function main() {
+  if (!process.env.TIGRIS_URI) {
+    console.log('Cannot find TIGRIS_URI environment variable ');
+    process.exit(1);
+  }
+  // setup client
+  const tigrisClient = new Tigris();
+  await tigrisClient.registerSchemas('models/tigris');
+}
+
+main();
